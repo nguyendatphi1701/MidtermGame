0ab046ec5432e52ae0669fb51f17769b48432523
asdasd <nguyendatphi1701@gmail.com>
2020-11-25 15:47:30 +0700

    ASdasdasd

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,15 +4,56 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float speed = 40f, maxspeed = 3, jumpPow = 220f;
+    public Rigidbody2D rigidbody2D;
+    public bool faceRight = true;
+    public Animator animator;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    private void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+        rigidbody2D.AddForce(Vector2.right * speed * h);
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+    
+                rigidbody2D.AddForce(Vector2.up * jumpPow);
+            
+        }
+        {
+            rigidbody2D.velocity = new Vector2(maxspeed, rigidbody2D.velocity.y);
+        }
+        if (rigidbody2D.velocity.x < -maxspeed)
+        {
+            rigidbody2D.velocity = new Vector2(-maxspeed, rigidbody2D.velocity.y);
+        }
+        if (h > 0 && !faceRight)
+        {
+            Flip();
+        }
+        if (h < 0 && faceRight)
+        {
+            Flip();
+        }
+    }
+    public void Flip()
+    {
+        faceRight = !faceRight;
+        Vector3 scale;
+        scale = transform.localScale;
+        scale.x *= -1;
+        this.transform.localScale = scale;
     }
 }
